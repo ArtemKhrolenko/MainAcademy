81f07fa7c6740351b85436a56eeeba29b623f2ef
artem.khrolenko <artem.khrolenko@gmail.com>
2019-03-05 10:33:41 +0200

    rename

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //chuxa
         }
     }
 }
