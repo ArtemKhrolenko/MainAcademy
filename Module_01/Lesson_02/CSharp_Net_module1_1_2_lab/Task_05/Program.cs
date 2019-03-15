3ab490b025433d5f9e914c29fd87a3e6b8139a11
artem.khrolenko <artem.khrolenko@gmail.com>
2019-03-15 21:59:43 +0200

    Finish Lesson2

--- Module_01/Lesson_02/CSharp_Net_module1_1_2_lab/Task_05/Program.cs
+++ Module_01/Lesson_02/CSharp_Net_module1_1_2_lab/Task_05/Program.cs
@@ -10,6 +10,21 @@ namespace Task_05
     {
         static void Main(string[] args)
         {
+            byte y;
+            int sum = 0;
+
+            for (y = 1; y <= 50; y++)
+            {
+                sum = sum + y;
+            }
+
+            Console.WriteLine($"Sum from 1 to 50 equals {sum}");
+            Console.ReadLine();
+
+            
+
+            
+
 
         }
     }
