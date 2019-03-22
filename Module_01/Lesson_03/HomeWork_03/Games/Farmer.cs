ed2a5fab9567033d97180977e5200a156b8001b4
artem.khrolenko <artem.khrolenko@gmail.com>
2019-03-22 10:57:03 +0200

    Finish Fermer

--- Module_01/Lesson_03/HomeWork_03/Games/Farmer.cs
+++ Module_01/Lesson_03/HomeWork_03/Games/Farmer.cs
@@ -9,7 +9,7 @@ namespace Games
     internal class Farmer
     {
 
-        private int currentChoice;
+        //private int currentChoice;
         private string whoAreOnTheBank; //String with staff
         private bool isOnTheRightBank;  //is farmer on the right bank?
         private string[] stuff = { "Wolf", "Goat", "Cabbage", "Farmer alone" };
@@ -17,13 +17,13 @@ namespace Games
         internal Farmer()
         {
             whoAreOnTheBank = "wgcf|0000";
-            isOnTheRightBank = false;
-            currentChoice = -1;
+            isOnTheRightBank = false;            
         }
 
         //display vars (choice)
         private void ShowVariants()
         {
+            Console.WriteLine(isOnTheRightBank ? "<======" : "======>");
             int j = isOnTheRightBank ? 5 : 0;
 
             for (int i = 0 + j; i < 4 + j; i++)
@@ -36,15 +36,16 @@ namespace Games
 
         private bool CheckTheChoice()
         {
+            StringBuilder strbCurrent, strbAnother;
             int _currentChoice = -1;
-            string _currentString = "";
+            string _currentBankString = "", _currentAnotherBankString = "";
             try
             {
                 //In dependency on where farmer is chose staff what on the exat bank
-                _currentString = isOnTheRightBank ? whoAreOnTheBank.Substring(5,4) : whoAreOnTheBank.Substring(0, 4);
+                _currentBankString = isOnTheRightBank ? whoAreOnTheBank.Substring(5,4) : whoAreOnTheBank.Substring(0, 4);
+                _currentAnotherBankString = isOnTheRightBank ? whoAreOnTheBank.Substring(0, 4) : whoAreOnTheBank.Substring(5, 4);
 
-                //Receive number of choice from user
-                Console.WriteLine("Please, make your choice!");
+                //Receive number of choice from user                
                 _currentChoice = Int32.Parse(Console.ReadLine());
 
                 //Checking for the valid of data
@@ -52,23 +53,23 @@ namespace Games
                 {
                     throw new Exception();
                 }
-                else if (_currentString[_currentChoice - 1] == '0')
+                else if (_currentBankString[_currentChoice - 1] == '0')
                 {
                     Console.WriteLine("This item is not on the bank!!!");
                     return false;
                 }                    
                 else //Choice is correct. Logic checking
                 {
-                    StringBuilder strb = new StringBuilder(_currentString);
-                    strb.Replace(strb[_currentChoice - 1], '0');                    
-                    Console.WriteLine(strb.ToString());
+                    strbCurrent = new StringBuilder(_currentBankString);
+                    strbCurrent.Replace(strbCurrent[_currentChoice - 1], '0', _currentChoice - 1, 1); //Remove the current stuff
+                    strbCurrent.Replace(strbCurrent[3], '0', 3, 1);   //Remove the farmer                                
 
                     Console.ForegroundColor = ConsoleColor.Red;
-                    if (strb.ToString() == "wg0f")
+                    if (strbCurrent.ToString() == "wg00")
                     {
                         Console.WriteLine("Oops! Wolf eats the Goat!");
                     }
-                    else if (strb.ToString() == "0gcf")
+                    else if (strbCurrent.ToString() == "0gc0")
                     {
                         Console.WriteLine("Oops! Goat eats the Cabbage!");
                     }
@@ -76,40 +77,33 @@ namespace Games
                     {
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Good Operation!!!!!");
+
+                        strbAnother = new StringBuilder(_currentAnotherBankString);
+                        strbAnother.Replace(strbAnother[_currentChoice - 1], _currentBankString[_currentChoice - 1], _currentChoice-1, 1); //Get the current stuff
+                        strbAnother.Replace(strbAnother[3], 'f', 3, 1); //Get the farmer
+
+                        //Invert the bank
+                        isOnTheRightBank = !isOnTheRightBank;
+
+                        //Combine the result BankString;
+                        whoAreOnTheBank = isOnTheRightBank ? strbCurrent.ToString() + "|" + strbAnother.ToString() : strbAnother.ToString() + "|" + strbCurrent.ToString();
+
+                        Console.WriteLine(whoAreOnTheBank);                        
+
                     }
                 }
 
             }
             catch
-            {                
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Incorrect data!");
                 return false;
             }      
 
             return true;
         }
-
-
-
-        //private void printVariants(ref bool _isBack)
-        //{
-        //    if (!_isBack)
-        //    {
-        //        Console.WriteLine("There: farmer and wolf - 1");
-        //        Console.WriteLine("There: farmer and cabbage - 2");
-        //        Console.WriteLine("There: farmer and goat - 3");
-        //        Console.WriteLine("There: farmer  - 4");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Back: farmer and wolf - 5");
-        //        Console.WriteLine("Back: farmer and cabbage - 6");
-        //        Console.WriteLine("Back: farmer and goat - 7");
-        //        Console.WriteLine("Back: farmer  - 8");                
-        //    }
-        //    Console.ForegroundColor = ConsoleColor.Blue;
-        //    Console.WriteLine("Please,  type numbers by step ");
-        //}
+        
         internal void StartGame()
         {
             #region The Task
@@ -121,24 +115,25 @@ At the same time can neither carry nor leave together on the banks of a wolf and
 a goat and cabbage. You can only carry a wolf with cabbage or as each passenger separately. 
 You can do whatever how many flights. How to transport the wolf, goat and cabbage that all went well?");
 
-            #endregion
-
-            
-            ShowVariants();
+            #endregion           
+           
 
             //Console.ForegroundColor = ConsoleColor.Blue;
             //Console.WriteLine("Please,  type numbers by step ");
-            Console.ForegroundColor = ConsoleColor.Gray;
+            
 
-            if (CheckTheChoice())
+            do
             {
-                //doSomeOperation
-            }
-            else
-            {
-                //continue
-            }
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("Please make a choice!!");
+                ShowVariants();
 
+                CheckTheChoice();
+            }
+            while (whoAreOnTheBank != "0000|wgcf");
+
+            Console.WriteLine("Congratulations!!! You WIN!!!");
+            Console.ReadKey();
 
         }
             
