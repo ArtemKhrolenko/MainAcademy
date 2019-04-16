ec3bd32c35106066588f8d2da70845894c68df5d
artem.khrolenko <artem.khrolenko@gmail.com>
2019-04-16 13:50:04 +0300

    FinishHomeWork03

---
+++ Module_02/Lesson_03/CSharp_Net_module1_2_3_lab/CSharp_Net_module1_2_3_lab/Money.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_2_3_lab
+{
+    // 1) declare enumeration CurrencyTypes, values UAH, USD, EU
+    enum CurrencyTypes
+    {
+        Uah,
+        Usd,
+        Eur
+    }
+
+    class Money
+    {
+        // 2) declare 2 properties Amount, CurrencyType
+        public Decimal Amount { get; set;}
+        public CurrencyTypes CurrencyType { get; set; }
+
+        
+        // 3) declare parameter constructor for properties initialization
+        public Money(Decimal amount, CurrencyTypes currencyType)
+        {
+            this.Amount = amount;
+            this.CurrencyType = currencyType;
+        }
+
+        public Money(Decimal amount)
+        {
+            this.Amount = amount;
+        }
+
+        // 4) declare overloading of operator + to add 2 objects of Money
+        public static Money operator +(Money moneyFirst, Money moneySecond)
+        {
+            return new Money(moneyFirst.Amount + moneySecond.Amount, moneyFirst.CurrencyType);
+        }
+
+        // 5) declare overloading of operator -- to decrease object of Money by 1
+        public static Money operator --(Money moneyOne)
+        {
+            return new Money(--moneyOne.Amount, moneyOne.CurrencyType);
+        }
+
+        //??????????????????
+        // 6) declare overloading of operator * to increase object of Money 3 times
+        public static Money operator *(Money moneyFirst, Money moneySecond)
+        {
+            return new Money(moneyFirst.Amount * moneySecond.Amount, moneyFirst.CurrencyType);
+        }
+
+        // 7) declare overloading of operator > and < to compare 2 objects of Money
+        public static bool operator >(Money moneyFirst, Money moneySecond)
+        {
+            return moneyFirst.Amount > moneySecond.Amount;
+        }
+
+        public static bool operator <(Money moneyFirst, Money moneySecond)
+        {
+            return moneyFirst.Amount < moneySecond.Amount;
+        }
+
+        // 8) declare overloading of operator true and false to check CurrencyType of object
+        public static bool operator true(Money money)
+        {
+            return money.CurrencyType is CurrencyTypes.Usd || money.CurrencyType is CurrencyTypes.Uah || money.CurrencyType is CurrencyTypes.Eur;
+        }
+
+        public static bool operator false(Money money)
+        {
+            return !(money.CurrencyType is CurrencyTypes.Usd && money.CurrencyType is CurrencyTypes.Uah && money.CurrencyType is CurrencyTypes.Eur);
+        }
+
+
+        // 9) declare overloading of implicit/ explicit conversion  to convert Money to double, string and vice versa
+        public static implicit operator Money(double doubleValue)
+        {
+            return new Money((decimal)doubleValue);
+        }
+
+        public static explicit operator double(Money money)
+        {
+            return (double)money.Amount;
+        }
+
+        public static implicit operator Money(string stringValue)
+        {
+            decimal.TryParse(stringValue, out var decimalVal);
+            return new Money(decimalVal);
+        }
+
+        public static explicit operator string(Money money)
+        {
+            return money.Amount.ToString();
+        }
+
+    }
+}
---
+++ Module_02/Lesson_03/CSharp_Net_module1_2_3_lab/CSharp_Net_module1_2_3_lab/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_2_3_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 10) declare 2 objects
+            Money moneyFirst  = new Money(10, CurrencyTypes.Eur);
+            Money moneySecond = new Money(100, CurrencyTypes.Eur);
+
+
+            // 11) do operations
+            // add 2 objects of Money
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Adding two objects money1 and money2...");
+            Console.ResetColor();
+            Console.WriteLine($"money1 (amount = {moneyFirst.Amount}) + money2 (amount = {moneySecond.Amount}) = money3 (amount = {(moneyFirst + moneySecond).Amount})");
+
+            // add 1st object of Money and double
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Adding objects money1 and double...");
+            Console.ResetColor();
+            double doubleVal = 5;
+            Console.WriteLine($"money1 (amount = {moneyFirst.Amount}) + double (doubleVal = {5}) = money3 (amount = {(moneyFirst + doubleVal).Amount})");
+
+            // decrease 2nd object of Money by 1 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Decreasing amount of money2...");
+            Console.ResetColor();
+            Console.WriteLine($"money2 (amount = {moneySecond.Amount}) = money3 (amount = {(--moneySecond).Amount})");
+
+            // increase 1st object of Money
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Increasing amount of money1...");
+            Console.ResetColor();
+            Console.WriteLine($"money1 (amount = {moneyFirst.Amount}) * money2 (amount = {moneySecond.Amount}) = money3 (amount = {(moneyFirst * moneySecond).Amount})"); //!!!!!! same object
+
+            // compare 2 objects of Money
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Comparision  money1 and money2...");
+            Console.ResetColor();
+            Console.WriteLine($"money1 (amount = {moneyFirst.Amount}) > money2 (amount = {moneySecond.Amount})? == {moneyFirst > moneySecond}");
+            Console.WriteLine($"money1 (amount = {moneyFirst.Amount}) < money2 (amount = {moneySecond.Amount})? == {moneyFirst < moneySecond}");
+
+
+            // compare 2nd object of Money and string
+            Console.ForegroundColor = ConsoleColor.Green;
+            string strValue = "22";
+            Console.WriteLine($"Comparision money2 and string ...");
+            Console.ResetColor();
+            Console.WriteLine($"money2 (amount {moneySecond.Amount}) > strValue {strValue} ? {moneySecond > strValue} ");
+            Console.WriteLine($"money2 (amount {moneySecond.Amount}) < strValue {strValue} ? {moneySecond < strValue} ");
+
+
+            // check CurrencyType of every object
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Checking currencyType of objects...");
+            Console.ResetColor();
+            Console.WriteLine(moneyFirst ? "Currency is valid" : "Currency is invalid");
+            Console.WriteLine(moneySecond ? "Currency is valid" : "Currency is invalid");
+
+
+            // convert 1st object of Money to string
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Converting money1 to string...");
+            Console.ResetColor();
+            string stringValue = (string)moneyFirst;
+            Console.WriteLine($"money1 (amount = {stringValue})");
+
+        }
+    }
+}
