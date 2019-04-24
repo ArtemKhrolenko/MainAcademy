de0e09f9ca98f0e5fa1c307bdf79aa6ff633d936
artem.khrolenko <artem.khrolenko@gmail.com>
2019-04-24 08:40:41 +0300

    HomeWork

---
+++ Module_02/Lesson_04/Hello_Class_stud/Hello_Class_stud/IMorse_crypt.cs
@@ -0,0 +1,13 @@
+﻿namespace Hello_Class_stud
+{
+    //Define interface IMorse_crypt with two methods  
+    //crypt - to crypt the string
+    //decrypt - to decrypt array of strings
+    public interface Imorse_crypt
+    {
+        string crypt(string stringItem);
+
+        string decrypt(string[] stringArray);
+    }
+
+}
---
+++ Module_02/Lesson_04/Hello_Class_stud/Hello_Class_stud/Morse_matrix.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading;
+
+namespace Hello_Class_stud
+{
+    //Implement class Morse_matrix derived from String_matrix, which realize IMorse_crypt
+    class Morse_matrix : String_matrix, Imorse_crypt
+    {
+        public const int Size_2 = Alphabet.Size;
+        private int offset_key = 0;
+
+        //Implement Morse_matrix constructor with the int parameter for offset
+        //Use fd(Alphabet.Dictionary_arr) and sd() methods
+
+        public Morse_matrix(int offset_key)
+        {
+            this.offset_key = offset_key;
+        }
+
+        public Morse_matrix(string[,] Dict_arr, int offset_key)
+        {
+            this.offset_key = offset_key;
+        }
+
+        public Morse_matrix()
+        {
+            fd(Alphabet.Dictionary_arr);
+        }
+
+
+
+        //Implement Morse_matrix constructor with the string [,] Dict_arr and int parameter for offset
+        //Use fd(Dict_arr) and sd() methods
+
+
+        //!Feel the matrix
+        private void fd(string[,] Dict_arr)
+        {
+            for (int ii = 0; ii < Size1; ii++)
+                for (int jj = 0; jj < Size_2; jj++)
+                    this[ii, jj] = Dict_arr[ii, jj];
+        }
+
+
+        //!Shifting colimn 1 of MAtrix with offset_key
+        private void sd()
+        {
+            int off = Size_2 - offset_key;
+            for (int jj = 0; jj < off; jj++)
+                this[1, jj] = this[1, jj + offset_key];
+            for (int jj = off; jj < Size_2; jj++)
+                this[1, jj] = this[1, jj - off];
+        }
+
+        //Implement Morse_matrix operator +
+
+
+        //Realize crypt() with string parameter
+        //Use indexers
+        public string crypt(string stringItem)
+        {
+            string resStr = String.Empty;
+            for (int i = 0; i < stringItem.Length; i++)
+            {
+                for (int j = 0; j < Size2; j++)
+                {
+                    if (Char.Parse(this[0, j]) == stringItem[i])
+                        resStr += this[1, j].ToString();
+                }
+            }
+            return resStr;
+        }
+
+        //Realize decrypt() with string array parameter
+        //Use indexers
+        public string decrypt(string[] stringArray)
+        {
+            string resStr = string.Empty;
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                for (int j = 0; j < Size2; j++)
+                {
+                    if (stringArray[i] == this[1, j])
+                        resStr += this[0, j];
+                }
+            }
+            return resStr;
+        }
+
+
+        //Implement Res_beep() method with string parameter to beep the string
+        internal void Res_beep(string strItemToBeep)
+        {
+            foreach (char beepItem in strItemToBeep.ToCharArray())
+            {
+
+                if (beepItem == '.')
+                {
+                    Console.Beep(1000, 100);
+                    Thread.Sleep(10);
+                }
+                else if (beepItem == '-')
+                {
+                    Console.Beep(1000, 250);
+                    Thread.Sleep(10);
+                }
+                else if (beepItem == ' ')
+                    Thread.Sleep(300);
+
+                Console.Write(beepItem);
+            }
+
+        }
+
+
+    }
+}
+
