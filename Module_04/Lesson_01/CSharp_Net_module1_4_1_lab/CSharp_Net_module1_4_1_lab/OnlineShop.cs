55a785e8b7c210e53810b83a9a25fd24c0d1f5c6
artem.khrolenko <artem.khrolenko@gmail.com>
2019-05-10 17:13:50 +0300

    HOMEWork

--- Module_04/Lesson_01/CSharp_Net_module1_4_1_lab/CSharp_Net_module1_4_1_lab/OnlineShop.cs
+++ Module_04/Lesson_01/CSharp_Net_module1_4_1_lab/CSharp_Net_module1_4_1_lab/OnlineShop.cs
@@ -9,16 +9,20 @@ namespace CSharp_Net_module1_4_1_lab
     class OnlineShop
     {
         // 4) declare event of type EventHandler<GoodsInfoEventArgs>
-       
+        public event EventHandler<GoodsInfoEventArgs> someEvent;
 
         // 5) declare method NewGoods for event initiation
         // use parameter string to get GoodsName
-        
+        public void NewGoods(string goodsName)
+        {
+            someEvent?.Invoke(this, new GoodsInfoEventArgs(goodsName));
+        }
+
         // don't forget to check if event is not null
         // in true case intiate the event
         // use next line
-       
+
         // your_event_name(this, new GoodsInfoEventArgs(GoodsName));
-       
-    }
+
+        }
 }
--- Module_04/Lesson_01/CSharp_Net_module1_4_1_lab/CSharp_Net_module1_4_1_lab/Program.cs
+++ Module_04/Lesson_01/CSharp_Net_module1_4_1_lab/CSharp_Net_module1_4_1_lab/Program.cs
@@ -11,15 +11,24 @@ namespace CSharp_Net_module1_4_1_lab
         static void Main(string[] args)
         {
             // 9) declare object of OnlineShop 
+            OnlineShop shop = new OnlineShop();
 
             // 10) declare several objects of Customer
+            Customer customer1 = new Customer("Jack");
+            Customer customer2 = new Customer("John");
+            Customer customer3 = new Customer("Jimm");
+
 
             // 11) subscribe method GotNewGoods() of every Customer instance 
             // to event NewGoodsInfo of object of OnlineShop
+            shop.someEvent += customer1.GotNewGoods;
+            shop.someEvent += customer2.GotNewGoods;
+            shop.someEvent += customer3.GotNewGoods;
 
             // 12) invoke method NewGoods() of object of OnlineShop
+            shop.NewGoods("Malina");
             // discuss results
-           
+
         }
     }
 }
