f5394f6e99ca32b99add0cad20bc6c1ad6202e6d
artem.khrolenko <artem.khrolenko@gmail.com>
2019-05-17 16:10:41 +0300

    Home Work

--- Module_04/Lesson_03/CSharp_Net_module1_4_3_lab/CSharp_Net_module1_4_3_lab/Animal.cs
+++ Module_04/Lesson_03/CSharp_Net_module1_4_3_lab/CSharp_Net_module1_4_3_lab/Animal.cs
@@ -1,32 +1,71 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace CSharp_Net_module1_4_3_lab
 {
     // 12) change methods of sorting to properties
 
     // 1) implement interface IComparable
-    public class Animal
+    public class Animal : IComparable
     {
         // 2) declare properties and parameter constructor
- 
+        public string Genus { get; set; }
+        public int Weight { get; set;   }
+
+        public Animal(string genus, int weight)
+        {
+            Genus = genus;
+            Weight = weight;
+        }
+
         // 3) implement method ComareTo()
         // it comares Genus of type string - return result of method String.Compare 
         // for this.genus and argument object
         // don't forget to cast object to Animal
- 
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Animal)) return -1;
+            else return string.Compare(Genus, ((Animal)obj).Genus);                    
+        }
 
         // 4) declare methods SortWeightAscending(), SortGenusDescending()
         // they are static and return IComparer
         // return type is custed from constructor of classes SortWeightAscendingHelper, 
         // SortGenusDescendingHelper calling 
-  
+
+
+        public static IComparer<Animal> SortWeightAccending()
+        {
+            return new SortWeightAscendingHelper();
+        }
+
+        public static IComparer<Animal> SortGenusDescending()
+        {
+            return new SortGenusDescendingHelper();
+        }
 
         // 5) declare 2 nested private classes SortWeightAscendingHelper, SortGenusDescendingHelper 
         // they implement interface IComparer
         // every nested class has implemented method Comare with 2 parameters of object and return int
-        // class SortWeightAscendingHelper sort weight by ascending
+        // class SortWeightAscendingHelper sort weight by ascending        
+
+        private class SortWeightAscendingHelper : IComparer<Animal>
+        {
+            public int Compare(Animal x, Animal y)
+            {
+                return x.Weight - y.Weight;
+            }
+        }
+
         // class SortGenusDescendingHelper sort genus by descending
- 
-      }
+        private class SortGenusDescendingHelper : IComparer<Animal>
+        {
+            public int Compare(Animal x, Animal y)
+            {
+                return string.Compare(y.Genus, x.Genus);
+            }
+        }
+
+    }
 }
--- Module_04/Lesson_03/CSharp_Net_module1_4_3_lab/CSharp_Net_module1_4_3_lab/Program.cs
+++ Module_04/Lesson_03/CSharp_Net_module1_4_3_lab/CSharp_Net_module1_4_3_lab/Program.cs
@@ -8,9 +8,42 @@ namespace CSharp_Net_module1_4_3_lab
         {
             // 10) Create an array of Animal objects and object of Animals    
             // print animals with foreach operator for object of Animals 
+           
+            Animal[] animals = { new Animal("Tiger", 180), new Animal("Lioin", 30), new Animal("Zebra", 120) };
+            Animals animalsObj = new Animals(animals);
+
+            Console.WriteLine("Objext od Animals: ");
+            foreach (Animal item in animalsObj)
+            {
+                Console.WriteLine($"Animal: {item.Genus} -- Weight: {item.Weight}");
+            }
+            Console.WriteLine(new string('-', 30));
 
             // 11) Invoke 3 types of sorting 
             // and print results with foreach operator for array of Animal objects  
+
+            Array.Sort(animals, Animal.SortGenusDescending());
+            foreach (var item in animals)
+            {
+                Console.WriteLine($"Animal: {item.Genus} -- Weight: {item.Weight}");
+            }
+            Console.WriteLine(new string('-', 30));
+
+
+            Array.Sort(animals, Animal.SortWeightAccending());
+            foreach (var item in animals)
+            {
+                Console.WriteLine($"Animal: {item.Genus} -- Weight: {item.Weight}");
+            }
+            Console.WriteLine(new string('-', 30));
+
+            Array.Sort(animals);
+            foreach (var item in animals)
+            {
+                Console.WriteLine($"Animal: {item.Genus} -- Weight: {item.Weight}");
+            }
+            Console.WriteLine(new string('-', 30));
+
 
 
             Console.ReadLine();
