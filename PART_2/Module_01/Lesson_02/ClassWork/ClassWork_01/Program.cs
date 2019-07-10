84d001e4ad106438cbd3b44c3ae327c431516f78
artem.khrolenko <artem.khrolenko@gmail.com>
2019-07-10 10:03:42 +0300

    SyncCommit

---
+++ PART_2/Module_01/Lesson_02/ClassWork/ClassWork_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+
+
+
+namespace BirdSpeciesLibrary
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = new Program().GetType();
+            t.GetCustomAttribute(Program.get);
+        }
+    }
+}
