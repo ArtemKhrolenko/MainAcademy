1930734301cd480466b04ce3077a3f91d66a98ef
artem.khrolenko <artem.khrolenko@gmail.com>
2019-09-17 21:42:56 +0300

    commit

---
+++ PART_2/Module_06/Lesson_02/WcfServiceLibrary1/WcfServiceLibrary1/IPictureService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+
+namespace WcfServiceLibrary1
+{
+    interface IPictureService
+    {
+         [OperationContract]
+        Models.Wrap_Picture GetPicture(int Id);
+        [OperationContract]
+        IEnumerable<Models.Wrap_Picture> GetPictureList();
+        [OperationContract]
+        void AddPicture(Models.Wrap_Picture picture);
+        [OperationContract]
+        void UpdatePicture(Models.Wrap_Picture picture);
+        [OperationContract]
+        void DeletePicture(Models.Wrap_Picture picture);
+        [OperationContract]
+        byte[] LoadImage(int Id);
+        [OperationContract]
+        void UploadImage(int Id, byte[] image);
+    }
+}
---
+++ PART_2/Module_06/Lesson_02/WcfServiceLibrary1/WcfServiceLibrary1/Models/Wrap_Picture.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using WcfServiceLibrary1.Models;
+
+namespace WcfServiceLibrary1.Models
+{
+    [DataContract]
+    public partial class Wrap_Picture
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public String Url { get; set; }
+        [DataMember]
+        public String Description { get; set; }
+        public byte[] foo { get; set; }
+
+        public static explicit operator Wrap_Picture(Models.Picture pict)
+        {
+            Wrap_Picture temp = new Wrap_Picture();
+            // code to convert 
+            temp.Id = pict.Id;
+            temp.Description = pict.Description;
+            temp.Url = pict.Url;
+            temp.foo = pict.foo;
+            return temp;
+        }
+
+    }
+
+    public partial class Picture
+    {
+        public static explicit operator Picture(Models.Wrap_Picture pict)
+        {
+            Picture temp = new Picture();
+            // code to convert 
+            temp.Id = pict.Id;
+            temp.Description = pict.Description;
+            temp.Url = pict.Url;
+            temp.foo = pict.foo;
+            return temp;
+        }
+    }
+
+    public partial class Picture
+    {
+        public int Id {get; set;}
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public byte[] foo { get; set; }
+    }
+}
